bfa3540731f5b841317013fc1c682e29916938ba
babyjisuu1010-maker <babyjisuu1010@gmail.com>
2026-06-22 20:15:58 +0700

    Update video.js

--- api/video.js
+++ api/video.js
@@ -1,9 +1,12 @@
 export default async function handler(req, res) {
   try {
+
     const apiKey = process.env.DOOD_API_KEY;
 
+    const page = req.query.page || 1;
+
     const response = await fetch(
-      `https://doodapi.co/api/file/list?key=${apiKey}`
+      `https://doodapi.co/api/file/list?key=${apiKey}&page=${page}`
     );
 
     const data = await response.json();
@@ -11,8 +14,10 @@ export default async function handler(req, res) {
     res.status(200).json(data);
 
   } catch (error) {
+
     res.status(500).json({
       error: error.message
     });
+
   }
 }
