e54b63369bee822c017c519717e59df9021e5b7d
babyjisuu1010-maker <babyjisuu1010@gmail.com>
2026-06-22 15:32:43 +0700

    Update videos.js

--- netlify/functions/videos.js
+++ netlify/functions/videos.js
@@ -1,12 +1,29 @@
 exports.handler = async () => {
 
-  const apiKey = process.env.DOOD_API_KEY;
+  try {
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      key_exists: !!apiKey
-    })
-  };
+    const apiKey = process.env.DOOD_API_KEY;
+
+    const response = await fetch(
+      `https://doodapi.com/api/account/info?key=${apiKey}`
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch(error) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+
+  }
 
 };
